9da67a22cee8eaad150c7cd70173497361b3a084
jason13official <jason13official@gmail.com>
2025-08-27 00:05:44 -0400

    minimize

--- common/src/main/java/com/example/examplemod/core/registry/ModContent.java
+++ common/src/main/java/com/example/examplemod/core/registry/ModContent.java
@@ -19,9 +19,6 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.PushReaction;
 
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.function.Supplier;
 
 public class ModContent implements IContentProvider {
@@ -31,38 +28,26 @@ public class ModContent implements IContentProvider {
 
     public static final Supplier<ItemStack> TAB_ICON = CachedSupplier.cache(() -> BuiltInRegistries.ITEM.get(identifier("new_torch")).getDefaultInstance());
 
-    private final Map<ResourceLocation, Block> allTorchesOrdered = new LinkedHashMap<>();
+    public static final Block torch = Services.PLATFORM.constructTorch(ParticleTypes.FLAME, BlockBehaviour.Properties.of().noCollission().instabreak().lightLevel((p_50886_) -> 14).sound(SoundType.WOOD).pushReaction(PushReaction.DESTROY));
+    public static final Block wallTorch = Services.PLATFORM.constructWallTorch(ParticleTypes.FLAME, BlockBehaviour.Properties.of().noCollission().instabreak().lightLevel((p_152607_) -> 14).sound(SoundType.WOOD).dropsLike(torch).pushReaction(PushReaction.DESTROY));
 
     @Override
     public void registerBlocks(Register<Block> registry) {
 
-        BlockBehaviour.Properties torchProperties = BlockBehaviour.Properties.of().noCollission().instabreak().lightLevel((p_50886_) -> 14).sound(SoundType.WOOD).pushReaction(PushReaction.DESTROY);
-
-        Block torch = Services.PLATFORM.constructTorch(ParticleTypes.FLAME, torchProperties);
         registry.add(TORCH_RL, torch);
-        allTorchesOrdered.put(TORCH_RL, torch);
 
-        BlockBehaviour.Properties wallTorchProperties = BlockBehaviour.Properties.of().noCollission().instabreak().lightLevel((p_152607_) -> 14).sound(SoundType.WOOD).dropsLike(torch).pushReaction(PushReaction.DESTROY);
-
-        Block wallTorch = Services.PLATFORM.constructWallTorch(ParticleTypes.FLAME, wallTorchProperties);
         registry.add(WALL_TORCH_RL, wallTorch);
-        allTorchesOrdered.put(WALL_TORCH_RL, wallTorch);
     }
 
     @Override
     public void registerItems(RegisterItem registry) {
-        Iterator<Map.Entry<ResourceLocation, Block>> it = allTorchesOrdered.entrySet().iterator();
 
-        while (it.hasNext()) {
-            Map.Entry<ResourceLocation, Block> block = it.next();
-            Map.Entry<ResourceLocation, Block> wallBlock = it.next();
-            registry.add(block.getKey().getPath(), new StandingAndWallBlockItem(block.getValue(), wallBlock.getValue(), new Item.Properties(), Direction.DOWN));
-        }
+        registry.add("new_torch", new StandingAndWallBlockItem(torch, wallTorch, new Item.Properties(), Direction.DOWN));
     }
 
     @Override
     public void registerItemTabs(RegisterItemTab registry) {
-        registry.add(Constants.MOD_ID, TAB_ICON, (parameters, output) -> output.accept(allTorchesOrdered.get(TORCH_RL)));
+        registry.add(Constants.MOD_ID, TAB_ICON, (parameters, output) -> output.accept(torch));
     }
 
     @Override
